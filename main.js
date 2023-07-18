072586bf760245080b67efee517941b15a8dbd8a
paulkani <paulkani.s@golden-hippo.com>
2023-07-18 20:16:01 +0530

    fibonacci series

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var n=prompt("enter value");
+var n=prompt("enter n value");
 var a=0;
 var b=1;
 var c=a+b;
@@ -7,5 +7,5 @@ for(let i=1;i<=n;i++)
 	c=a+b;
 	a=b;
 	b=c;
+	document.write(c+" ");
         }
- 	document.write(c);
